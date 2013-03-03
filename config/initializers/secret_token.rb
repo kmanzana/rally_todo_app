5471a03439b363f86d3079665aee481b0a603273
Kelton Manzanares <kmanzana@mines.edu>
2013-03-02 19:32:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RallyTodoApp::Application.config.secret_token = 'a701389fc8188afb28910efe5fea4078978688286fcdfd3ff4a8521ab3c6abee555039d02b4e576dca48bd7ddca164053f9985ea656932a683376d74abeeb6d9'
